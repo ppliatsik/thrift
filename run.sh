bcecde9484d343bf12997a94133e707cab9ec16f
ppliatsik@gmail.com <6977plia977=>
2019-08-27 10:32:14 +0300

    run

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir thrift-client/src/main/resources/
 ~/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic panos
 
 mvn install
